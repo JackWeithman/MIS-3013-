550c55237bdd95358ea4277f4ec157c836a1adba
jackweithman <jack.r.weithman-1@ou.edu>
2020-09-01 12:14:01 -0500

    Finished the input/output example for class

--- InClassExamples/InputOutput/Program.cs
+++ InClassExamples/InputOutput/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿/*
+*
+*@Author:Professor Ackerman
+*Collaboration with: the class
+*/
+using System;
 
 namespace InputOutput
 {
     class Program
     {
+        const string CLASS_NAME = "MIS 3013";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // this wont work bc its a constant CLASS_NAME = "MIS 3353";
+            //string firstname, lastname; 
+            string firstname = "Jack";
+            string lastname = "Weithman";
+
+            Console.WriteLine("Please enter your first name >>");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("What is your GPA? >>");
+            string gpaInput = Console.ReadLine();
+            double gpa = Convert.ToDouble(gpaInput);
+
+            //This doesn't make sence BUT shows the calculations
+            double gpaEnhanced = gpa + 1.5;
+
+            Console.WriteLine(firstname + " " + lastname +
+                " is learning " + CLASS_NAME + " with a GPA of " + gpa.ToString("N2"));
         }
     }
 }
