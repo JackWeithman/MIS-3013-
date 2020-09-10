ba2051a48a9b8080886536bd1a7a60c43d3d823f
jackweithman <jack.r.weithman-1@ou.edu>
2020-09-10 12:10:35 -0500

    Finished coin toss with kyle brandon

---
+++ CoinToss/CoinToss/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CoinToss
+{
+    class Program
+        
+    {
+        static void Main(string[] args)
+        {
+            const string DEVELOPER = "Jack Weithman";
+            const int Heads = 1;
+            const int Tails = 0;
+            Console.WriteLine("Heads or Tails?");
+            string answer = Convert.ToString(Console.ReadLine());
+
+            Random rnd = new Random();
+            int result = rnd.Next(0, 2);
+
+            if (result == Heads && answer == "Heads")
+            {
+                Console.WriteLine("It was Heads");
+            }
+            else if (result == Tails && answer == "Tails")
+            {
+                Console.WriteLine("It was Tails");
+            }
+            else
+            {
+                Console.WriteLine("You were WRONG");
+            }
+            Console.WriteLine(DEVELOPER);
+        }
+    }
+}
